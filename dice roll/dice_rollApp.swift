a4aa162e3e984bd26187423e92d781f683b28a8b
will lutz <wlutz@bsd220.org>
2023-10-25 09:46:06 -0500

    Initial Commit

---
+++ dice roll/dice_rollApp.swift
@@ -0,0 +1,17 @@
+//
+//  dice_rollApp.swift
+//  dice roll
+//
+//  Created by will lutz on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct dice_rollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
