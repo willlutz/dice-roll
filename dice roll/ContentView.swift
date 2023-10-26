4d957ffbd5620ff0a01c94b8eaaafffb55cc61f0
will lutz <wlutz@bsd220.org>
2023-10-26 09:57:26 -0500

    roll

--- dice roll/ContentView.swift
+++ dice roll/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var randomValue = 0
+    @State private var rotation = 0
     var body: some View {
         VStack {
             Text("Dice Roll")
@@ -17,9 +18,15 @@ struct ContentView: View {
             Image("pips \(randomValue)")
                 .resizable()
             .frame(width: 200, height: 200, alignment: .center)
+            .rotationEffect(.degrees(Double(rotation)))
+            .rotation3DEffect(.degrees(Double(rotation)), axis: (x: 1, y: 1, z: 0))
             .padding ()
             .onTapGesture {
                 randomValue = Int.random(in: 1...6)
+                withAnimation(.default) {
+                                        rotation += 360
+                                    }
+
             }
             Spacer ()
         }
